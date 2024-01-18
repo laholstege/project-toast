daf60b0d85000a8b5a1cf557fbdf6641629f51ef
River Holstege <laholstege@ucdavis.edu>
2024-01-17 17:54:45 -0800

    oops forgot one

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -5,16 +5,12 @@ import styles from './ToastShelf.module.css';
 import { ToastContext } from '../ToastProvider';
 
 function ToastShelf() {
-  const { toasts, dismissToast } = React.useContext(ToastContext);
+  const { toasts } = React.useContext(ToastContext);
   return (
     <ol className={styles.wrapper}>
       {toasts.map((toast) => (
         <li className={styles.toastWrapper} key={toast.id}>
-          <Toast
-            id={toast.id}
-            variant={toast.variant}
-            handleDismiss={dismissToast}
-          >
+          <Toast id={toast.id} variant={toast.variant}>
             {toast.message}
           </Toast>
         </li>
